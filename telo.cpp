96dc7bbfef0b1eb851855342a41a097646cc53e6
shabit24 <hossain.shabit24@gmail.com>
2023-03-06 16:07:35 +0700

    firts commit

---
+++ telo.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     float len, bre, area;
+//     cout << "Enter length of rectangle: ";
+//     cin >> len;
+//     cout << "Enter the breadth of rectangle";
+//     cin >> bre;
+//     area = len * bre;
+//     cout << "\nArea = " << area;
+//     cout << endl;
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float len, bre;
+    cout << "Enter Length of Rectangle:";
+    cin >> len;
+    cout << "Enter Breadth of Rectangle:";
+    cin >> bre;
+    area = areaOfRectangle(len, bre)
+}
